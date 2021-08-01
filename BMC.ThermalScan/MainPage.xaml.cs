1d8f674daeb579d4034b6195d5d614125199fcac
Mif Masterz <mifmasterz@yahoo.com>
2021-08-01 13:48:55 +0700

    add pbi support thermal

--- BMC.ThermalScan/MainPage.xaml.cs
+++ BMC.ThermalScan/MainPage.xaml.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
 using Windows.Foundation;
@@ -64,11 +66,36 @@ namespace BMC.ThermalScan
         {
 
         }
-
+        async void dispatcherTimer_Tick(object sender, object e)
+        {
+            if (dataTemp == null) return;
+            if (client == null) client = new HttpClient();
+            var json = JsonConvert.SerializeObject(dataTemp);
+            var res = await client.PostAsync(UrlPbi, new StringContent(json));
+            if ((res.IsSuccessStatusCode))
+            {
+                await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, async () =>
+                {
+                    TxtStatus.Text = $"{DateTime.Now} - Sukses kirim data ke PBI";
+                });
+            }
+            else
+            {
+                await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, async () =>
+                {
+                    TxtStatus.Text = $"{DateTime.Now} - Gagal kirim data ke PBI";
+                });
+            }
+        }
         private async void Page_Loaded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
         {
             await ThermCam.InitThermalCamera();
+            //set timer 
 
+            dispatcherTimer = new DispatcherTimer();
+            dispatcherTimer.Tick += dispatcherTimer_Tick;
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 5);
+            dispatcherTimer.Start();
             var t = Task.Run(async () =>
             {
                 while (true)
@@ -97,6 +124,13 @@ namespace BMC.ThermalScan
                         // 手順３ 採った温度データと画像をUIにセット
                         imageMain.Source = bmp;
                         tbTemparature.Text = center.Average().ToString("F2") + " ℃";
+                        if (dataTemp == null)
+                        {
+                            dataTemp = new SensorData();
+                            dataTemp.Sensor = "MLX90640";
+                        }
+                        dataTemp.Suhu = (float)center.Average();
+                        dataTemp.Waktu = DateTime.Now;
                     });
 
                     // 手順４ 二週目の前にとりあえずDelay(1)
@@ -104,11 +138,25 @@ namespace BMC.ThermalScan
                 }
             });
         }
-
+        string UrlPbi = "https://api.powerbi.com/beta/e4a5cd36-e58f-4f98-8a1a-7a8e545fc65a/datasets/079c3613-7862-447e-ad17-6a93a62eeb6f/rows?key=QrN9qUl0VHnneWX2Kw%2BUfE4Bk4f0MJ1eihKYfSWpy9WxlscUmPzYUw7%2FunPVW1PXjt7AjN%2Fstb0LdRH0JFkN%2Fg%3D%3D";
+        static DispatcherTimer dispatcherTimer;
+        static SensorData dataTemp;
+        static HttpClient client;
         private void MainPage_Unloaded(object sender, object args)
         {
             ThermCam.Dispose();
             ThermCam = null;
         }
     }
+
+
+   
+
+    public class SensorData
+    {
+        public DateTime Waktu { get; set; }
+        public float Suhu { get; set; }
+        public string Sensor { get; set; }
+    }
+
 }
